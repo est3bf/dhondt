5147e3461ff3838fe218535cf1c81a1c01476054
Esteban Faye <est3ban.faye@gmail.com>
2024-12-02 11:44:07 -0300

    feat: Generate doc using openapi yaml and update api.
    Change app for automatic doc generation from openapi specification file (yaml)
    Update api routes (skeleton) from initial design.

--- src/web/api/api.py
+++ src/web/api/api.py
@@ -1,7 +1,83 @@
 import logging
 
+from flask import abort, jsonify
+from flask.views import MethodView
 from flask_smorest import Blueprint
+from marshmallow import ValidationError
 
 logger = logging.getLogger(__name__)
 
 blueprint = Blueprint("dhondt", __name__, description="D'hondt method API")
+
+
+########################
+# Configurations
+########################
+@blueprint.route("/dhondt/v1/districts")
+class DistrictsRoute(MethodView):
+    def get(self):
+        pass
+
+
+@blueprint.route("/dhondt/v1/districts/<districtId>")
+class DistrictRoute(MethodView):
+    def get(self, districtId):
+        pass
+
+
+@blueprint.route("/dhondt/v1/districts/<districtId>/political-party-lists")
+class PoliticalPartyListsRoute(MethodView):
+    def get(self, districtId):
+        pass
+
+    def post(self, payload, districtId):
+        pass
+
+
+@blueprint.route("/dhondt/v1/districts/<districtId>/political-party-lists/<pplistId>")
+class PoliticalPartyListRoute(MethodView):
+    def get(self, districtId, pplistId):
+        pass
+
+    def put(self, parameters, districtId, pplistId):
+        pass
+
+
+@blueprint.route("/dhondt/v1/districts/<districtId>/scrutinies")
+class ScrutiniesRoute(MethodView):
+    def get(self, districtId):
+        pass
+
+    def post(self, payload, districtId):
+        pass
+
+
+@blueprint.route("/dhondt/v1/districts/<districtId>/scrutinies/<scrutinyId>")
+class ScrutinyRoute(MethodView):
+    def get(self, districtId, scrutinyId):
+        pass
+
+
+########################
+# Upgrading votes
+########################
+@blueprint.route(
+    "/dhondt/v1/districts/<districtId>/political-party-lists/<pplistId>/vote",
+    methods=["PUT"],
+)
+def upgrade_vote(parameters, districtId, pplistId):
+    pass
+
+
+########################
+# Seats Result
+########################
+@blueprint.route(
+    "/dhondt/v1/districts/<districtId>/scrutinies/<scrutinyId>/seats-status"
+)
+class CalculateSeatsRoute(MethodView):
+    def get(self, parameters, districtId, scrutinyId):
+        pass
+
+    def post(self, districtId, scrutinyId):
+        pass
--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+from pathlib import Path
+import yaml
 import logging
 
 from flask import Flask
+from apispec import APISpec
 from flask_smorest import Api
 
 from dhondt.web.api.config import BaseConfig
 import dhondt.web.api.api as api
+
+
+SPEC_NAMEFILE = "dhondt.yaml"
+
 
 app = Flask(__name__)
 
@@ -13,11 +20,22 @@ app.config.from_object(BaseConfig)
 
 dhondt_api = Api(app)
 
-logging.basicConfig(level=logging.DEBUG,  
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 # Logger de la aplicación
 logger = logging.getLogger(__name__)
 
 # register api
 dhondt_api.register_blueprint(api.blueprint)
+
+# load openapi specification
+api_spec = yaml.safe_load((Path(__file__).parent / SPEC_NAMEFILE).read_text())
+spec = APISpec(
+    title=api_spec["info"]["title"],
+    version=api_spec["info"]["version"],
+    openapi_version=api_spec["openapi"],
+)
+spec.to_dict = lambda: api_spec
+dhondt_api.spec = spec
